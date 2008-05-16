6a0b391c5e4d11d2248eafe012ae9da15bc30864
David Winter <i@davidwinter.me.uk>
2008-05-16 20:14:20 +0100

    Complete rewrite. Basically function complete.
    Need to now work on preferences store for username, password and check interval.

--- gmail-notify.py
+++ gmail-notify.py
@@ -1,91 +1,104 @@
 #!/usr/bin/env python
 
 import gtk
-import feedparser
-import urllib
-import sys
-import getpass
+import gobject
 import webbrowser
 
+import urllib 
+import feedparser
+
+import datetime
+
+
 class GmailView:
+    
+    def __init__(self, control):
+        self.control = control
 
-    def __init__(self):
-
-        self.status_icon = gtk.status_icon_new_from_file('no_new.ico')
-        self.status_icon.set_tooltip('No new email')
-        self.status_icon.set_visible(True)
-
+        self.mail_items = []
+        
+        """Set-up Menu"""
         self.menu = gtk.Menu()
+        self.menu.append(gtk.SeparatorMenuItem())
         self.about_item = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
         self.menu.append(self.about_item)
         self.quit_item = gtk.ImageMenuItem(gtk.STOCK_QUIT)
         self.menu.append(self.quit_item)
 
-    def new_mail(self):
+        self.quit_item.connect('activate', gtk.main_quit)
+
+        """Set-up Status Icon"""
+        self.status_icon = gtk.status_icon_new_from_file('no_new.ico')
+        self.status_icon.set_visible(True)
         
+        self.status_icon.connect(   'popup-menu', 
+                                    self.control.on_menu_popup,
+                                    self.menu)
+
+        self.status_icon.connect('activate', self.control.go_to_inbox)
+
+        self.control.check_mail(self)
+        
+        gobject.timeout_add(5 * 60 * 1000, self.control.check_mail, self)
+
+    """Set icon, and take dictionary to build menu items."""
+    def new_mail(self, list):
+        list.reverse()
+        for email in list:
+            item = GmailMessageItem(email['summary'], email['url'])
+            item.connect('activate', self.control.open_email)
+            self.menu.prepend(item)
+            self.mail_items.append(item)
+
+        print "New Mail" + " (" + str(datetime.datetime.now()) + ")"
+        self.status_icon.set_tooltip('New Mail')
         self.status_icon.set_from_file('unread.ico')
 
+    """Set icon, and remove all menu items besides About and Quit."""
     def no_mail(self):
-        
-        self.status_icon.set_from_file('no_new.ico')
+        for item in self.mail_items:
+            self.menu.remove(item)
 
-    def new_mail_menu_item(self, label, url):
-        
-        return GmailMessageItem(label, url)
-        
-    
+        print "No Mail" + " (" + str(datetime.datetime.now()) + ")"
+        self.status_icon.set_tooltip('No Mail')
+        self.status_icon.set_from_file('no_new.ico') 
+
 
 class GmailController:
 
     def __init__(self):
-
-        self.view = GmailView()
-        self.view.quit_item.connect('activate', self.quit)
-        self.view.status_icon.connect('popup-menu', self.menu_show)
-
         self.gmail = GmailChecker()
-        
-        print self.gmail.unread_count()
-        print self.gmail.feed.entries[0].author_detail.name
-        print self.gmail.feed.entries[0].link
-        #entry = self.gmail.feed.entries[0]
-        #label = entry.author_detail.name + ' ' + entry['title']
-        #url = entry.link
-        #new_item = GmailMessageItem(label, url)
-        #new_item.connect('activate', self.open_message)
-        #self.view.menu.prepend(new_item)
-
-        self.check_mail()
-        
-    def open_message(self, widget):
-        webbrowser.open_new_tab(widget.url)
     
-    def check_mail(self):
-        self.gmail.parse()
-
-        if self.gmail.unread_count() > 0:
-            message_builder = GmailMessageBuilder()
-            emails = message_builder.build_label_url_dict(self.gmail.feed)
-            for email in emails:
-                new_item = GmailMessageItem(emails[email], email)
-                new_item.connect('activate', self.open_message)
-                self.view.menu.prepend(new_item)
-            
-
-            self.view.new_mail()
-        else:
-            self.view.no_mail()
-
-    def menu_show(self, widget, button, time):
-
-        if button == 3:
-            self.view.menu.show_all()
-            self.view.menu.popup(None, None, None, button, time)
-            
-    def quit(self, widget):
+    def check_mail(self, view):
+        feed = self.gmail.feed()
         
-        self.view.status_icon.set_visible(False)
-        gtk.main_quit()
+        if len(feed.entries) > 0:
+            list = []
+
+            """Reversing list so that when added to menu they are in
+            correct order."""
+            for entry in feed.entries:
+                value = entry.author_detail.name + ': ' + entry.title
+                url = entry.link
+                email = {'summary': value, 'url': url}
+                list.append(email)
+            
+            view.new_mail(list)
+
+        else:
+            view.no_mail()
+         
+    def on_menu_popup(self, widget, button, time, menu):
+        if button == 3:
+            menu.show_all()
+            menu.popup(None, None, None, button, time)
+    
+    def open_email(self, item):
+        webbrowser.open_new_tab(item.url)
+
+    def go_to_inbox(self, item):
+        webbrowser.open_new_tab('http://mail.google.com/mail')
+
 
 class GmailMessageBuilder:
 
@@ -97,23 +110,19 @@ class GmailMessageBuilder:
 
         return dict
 
+
 class GmailChecker:
 
     feed_realm = 'New mail feed'
     feed_domain = 'mail.google.com'
     feed_url = 'http://mail.google.com/mail/feed/atom'
 
-    def __init__(self):
-        self.parse()
-
-    def parse(self):
+    def feed(self):
         opener = GmailURLOpener()
         f = opener.open(self.__class__.feed_url)
-        self.feed_src = f.read()
-        self.feed = feedparser.parse(self.feed_src)
+        feed_src = f.read()
+        return feedparser.parse(feed_src)
 
-    def unread_count(self):
-        return len(self.feed.entries)
 
 class GmailURLOpener(urllib.FancyURLopener):
 
@@ -121,6 +130,7 @@ class GmailURLOpener(urllib.FancyURLopener):
         username = open('username').read().strip()
         password = open('password').read().strip()
         return (username, password)
+
 
 class GmailMessageItem(gtk.MenuItem):
     
@@ -130,7 +140,6 @@ class GmailMessageItem(gtk.MenuItem):
 
 if __name__ == '__main__':
     
-    app = GmailController()
+    app = GmailView(GmailController())
     gtk.main()
-
 
